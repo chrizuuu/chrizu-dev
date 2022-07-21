6a76bbf3154aa7e0ff471289eb1571a69f33dfb1
Krzysztof Bonecki <krzysztofboneckikontakt@gmail.com>
2022-07-21 20:11:43 +0200

    Add hover animation to StyledLink

--- src/components/StyledLink/StyledLink.tsx
+++ src/components/StyledLink/StyledLink.tsx
@@ -12,8 +12,14 @@ function StyledLink({
   color?: string;
 }): JSX.Element {
   return (
-    <Link to={route}>
-      <span className={classnames("tracking-widest", color)}>{text}</span>
+    <Link
+      to={route}
+      className={
+        "group flex flex-col justify-center items-center gap-0 h-[40px] hover:translate-y-[-4px] transition-transform duration-200"
+      }
+    >
+      <span className={classnames("tracking-widest ", color)}>{text}</span>
+      <span className="w-0 h-[1px] bg-black-800 transition-w duration-500 group-hover:w-full"></span>
     </Link>
   );
 }
