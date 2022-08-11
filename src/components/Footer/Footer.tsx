ba77384b1efe5711c5cb65918e04a8201d221ddb
Krzysztof Bonecki <krzysztofboneckikontakt@gmail.com>
2022-08-11 19:34:25 +0200

    Add copyable email to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import AnimatedHeader from "components/Texts/AnimatedHeader";
 import SocialList from "components/SocialList/SocialList";
 import { Link } from "gatsby";
+import classNames from "classnames";
 
 function FooterContact() {
   return (
@@ -26,17 +27,64 @@ function FooterContact() {
       <h3 className="text-white-700 text-lg sm:text-3xl text-center block pb-[20px]">
         Got a project or proposal?
       </h3>
-      <Link to="/" className="group">
+      <a href="mailto:chrizudev@gmail.com" className="group">
         <AnimatedHeader
           fontSize="text-4xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-9xl"
           className="text-white-900 after:w-full after:bg-white-900 after:h-1 after:block after:group-hover:scale-x-0 after:duration-500"
         >
           {"ContactMe"}
         </AnimatedHeader>
-      </Link>
+      </a>
     </motion.div>
   );
 }
+
+function CopyableEmail() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  return (
+    <div
+      onMouseLeave={() => setIsCopied(false)}
+      className="group pb-[10px] lg:pb-0 lg:order-3 group overflow-hidden"
+    >
+      <div className="flex flex-col justify-center items-center lg:items-end relative">
+        <div className="flex flex-col items-end text-white-700 text-[12px] uppercase">
+          <div
+            className={classNames(
+              "transition-all duration-300",
+              isCopied
+                ? "opacity-0 -translate-y-full"
+                : "opacity-0 group-hover:opacity-60"
+            )}
+          >
+            Copy email
+          </div>
+          <div
+            className={classNames(
+              "absolute transition-all duration-300",
+              isCopied
+                ? "opacity-60 translate-y-0"
+                : "opacity-0 -translate-y-full"
+            )}
+          >
+            Copied email
+          </div>
+        </div>
+        <button
+          type="button"
+          onClick={() => {
+            navigator.clipboard.writeText("chrizudev@gmail.com");
+            setIsCopied(!isCopied);
+          }}
+          className="text-white-900"
+        >
+          chrizudev@gmail.com
+        </button>
+      </div>
+    </div>
+  );
+}
+
 function SubFooter() {
   return (
     <motion.div
@@ -57,11 +105,7 @@ function SubFooter() {
       className="flex flex-col gap-y-2 items-center justify-between lg:flex-row px-defaultSpacing"
     >
       <SocialList color="white-900 lg:order-1" />
-      <a className="lg:order-3" href="mailto:chrizudev@gmail.com">
-        <span className="text-white-900 pb-[10px] lg:pb-0">
-          chrizudev@gmail.com
-        </span>
-      </a>
+      <CopyableEmail />
       <span className="text-white-900  pb-[10px] lg:pb-0 lg:order-2">
         © 2022 Krzysztof Bonecki
       </span>
